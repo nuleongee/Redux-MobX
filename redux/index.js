6c79776e6ff0582838c731b22937d3742a7281fd
emsbn <emsbn1@gmail.com>
2021-01-09 23:59:44 +0900

    action, reducer

--- redux/index.js
+++ redux/index.js
@@ -1,6 +1,15 @@
 const { createStore } = require('redux');
 
-const reducer = () => {};
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'CHANGE_COMP_A':
+      return {
+        compA: action.data,
+        compB: 12,
+        compC: null,
+      };
+  }
+};
 const initialState = {
   compA: 'a',
   compB: 12,
@@ -8,5 +17,17 @@ const initialState = {
 };
 
 const store = createStore(reducer, initialState);
+
+console.log(store.getState());
+
+// action
+const changeCompA = (data) => {
+  return {
+    type: 'CHANGE_COMP_A',
+    data,
+  };
+};
+
+store.dispatch(changeCompA('b'));
 
 console.log(store.getState());
