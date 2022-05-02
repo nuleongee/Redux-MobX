4c3825017c75a888354542bb1da8aecc954b6fe9
nuleongee <nuleongee@gmail.com>
2022-05-02 14:28:20 +0900

    action, action creator, reducer

--- redux/images/index.js
+++ redux/images/index.js
@@ -1,6 +1,27 @@
 const { createStore } = require('redux');
 
-const reducer = () => {};
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'CHANGE_COMP_A':
+      return {
+        compA: action.data,
+        compB: prevState?.compB ?? initState.compB,
+        compC: prevState?.compC ?? initState.compC,
+      };
+    case 'CHANGE_COMP_B':
+      return {
+        compA: prevState?.compA ?? initState.compA,
+        compB: action.data,
+        compC: prevState?.compC ?? initState.compC,
+      };
+    case 'CHANGE_COMP_C':
+      return {
+        compA: prevState?.compA ?? initState.compA,
+        compB: prevState?.compB ?? initState.compB,
+        compC: action.data,
+      };
+  }
+};
 const initState = {
   compA: 'a',
   compB: 12,
@@ -9,4 +30,17 @@ const initState = {
 
 const store = createStore(reducer, initState);
 
-console.log(store.getState());
+console.log('1st', store.getState());
+
+// action creator
+const changeCompA = (data) => {
+  // action
+  return {
+    type: 'CHANGE_COMP_A',
+    data,
+  };
+};
+
+store.dispatch(changeCompA('b'));
+
+console.log('2nd', store.getState());
